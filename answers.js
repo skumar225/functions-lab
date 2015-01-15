d4c744b43bc8a3cf0686085fa7bbb45ad8f5e74c
Shilpa Kumar <skumar225@gmail.com>
2015-01-15 09:01:27 -0800

    initial commit

--- answers.js
+++ answers.js
@@ -1,1 +1,61 @@
-console.log(sweet)
+//#1
+function maxOfTwoNumbers(x, y) {
+	if (x > y) {
+		return console.log(x);
+	}
+
+	else {
+		return console.log(y);
+	}
+}
+
+maxOfTwoNumbers(4,3);
+
+
+//#2
+
+function maxOfThree (x, y, z) {
+	if ((x > y) && (x > z)) {
+		return console.log(x)
+	} 
+
+	else if ((y > x) && (y >z)) {
+		return console.log(y);
+	}
+
+	else ((z > x) && (z > y)) {
+		return console.log(z);
+	}
+}
+
+maxOfThree(2, 3, 4);
+
+
+//#3
+
+function vowel (letter) {
+	if (letter === "a" || "e" || "i" || "o" || "u") {
+		return true;
+	}
+
+	else {
+		return false;
+	}
+}
+
+
+//#4
+var arry = [];
+
+function sumArray (arry) { 
+	for (var i = 0, sum = 0; i < arry.length; sum += arry [i++]) {
+		return i;
+	}
+
+}
+
+function multiplyArray (arry) { 
+	for (var i = 0, multipy = 0; i < arry.length; multipy *= arry [i++]) {
+		return i;
+	}
+}
